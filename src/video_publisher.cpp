073e3fa5f2d474cedb1dc53484e5e80b268c6dc4
lucianovr <rluciano025@gmail.com>
2018-05-22 21:38:08 -0300

    atualizando launch file

--- src/video_publisher.cpp
+++ src/video_publisher.cpp
@@ -2,47 +2,82 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
-#include <sstream> // for converting the command line parameter to integer
+#include <std_msgs/Empty.h>
+#include <std_msgs/Header.h>
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "image_publisher");
     ros::NodeHandle nh;
+    ros::NodeHandle _nh("~"); // to get the private params
     image_transport::ImageTransport it(nh);
-    image_transport::Publisher pub = it.advertise("camera/image", 1);
+    image_transport::Publisher pub = it.advertise("camera", 1);
 
     int camera_id;
-    if (!ros::param::get("~camera_id", camera_id))
+    cv::VideoCapture cap;
+    if (_nh.getParam("camera_id", camera_id))
     {
-		ROS_ERROR("Could not get the parameter camera_id\n.");
-		return 1;
+        ROS_INFO_STREAM("Getting video from provider: /dev/video" << camera_id);
+        cap.open(camera_id);
+    }else{
+        ROS_ERROR("Failed to get param 'camera_id'");
+        return -1;
     }
-    cv::VideoCapture cap(camera_id);
-    if(!cap.isOpened()) 
-    {
-		ROS_ERROR("Could not open the camera with id %d.", camera_id);
-		return 1;
-    }
-    cap.set(CV_CAP_PROP_FRAME_WIDTH,640);
-    cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
 
-    cv::Mat frame;
+
+    int fps;
+    _nh.param("fps", fps, 240);
+    ROS_INFO_STREAM("Throttling to fps: " << fps);
+
+
+    int width_target;
+    int height_target;
+    _nh.param("width", width_target, 0);
+    _nh.param("height", height_target, 0);
+    if (width_target != 0 && height_target != 0){
+        ROS_INFO_STREAM("Forced image width is: " << width_target);
+        ROS_INFO_STREAM("Forced image height is: " << height_target);
+    }
+
+    if(!cap.isOpened()){
+        ROS_ERROR_STREAM("Could not open the stream.");
+        return -1;
+    }
+    if (width_target != 0 && height_target != 0){
+        cap.set(CV_CAP_PROP_FRAME_WIDTH, width_target);
+        cap.set(CV_CAP_PROP_FRAME_HEIGHT, height_target);
+    }
+
+    int cvt2gray;
+    _nh.param("cvt2gray", cvt2gray, 0);
+
+
+    ROS_INFO_STREAM("Opened the stream, starting to publish.");
+
+    cv::Mat frame, gray;
     sensor_msgs::ImagePtr msg;
-
-	int rate;
-	if (!ros::param::get("~loop_rate", rate))
-    {
-		ROS_ERROR("Could not get the parameter rate\n.");
-		return 1;
-    }
-    ros::Rate loop_rate(rate);
+    std_msgs::Header header;
+	
+    ros::Rate loop_rate(fps);
     while (nh.ok()) {
 		cap >> frame;
-		if(!frame.empty()) {
-			msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
-			pub.publish(msg);
-		}
-		loop_rate.sleep();
+        if (pub.getNumSubscribers() > 0)
+        {
+		    if(!frame.empty()) {
+                if (cvt2gray != 0)
+                {
+		    	    cv::cvtColor(frame, gray, CV_BGR2GRAY);
+		    	    msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", gray).toImageMsg();
+                }
+                else
+                {
+                    msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+                }
+                pub.publish(msg);
+		    }
+            ros::spinOnce();
+        }
+        loop_rate.sleep();
     }
     cap.release();
     return 0;
