c4dcae963b738bbc29308077fd14e54bf2aac290
lvribeiro <lucianoribeiro@get.inatel.br>
2018-09-10 22:03:47 -0300

    alterando subscriber

--- src/image_subscriber.cpp
+++ src/image_subscriber.cpp
@@ -2,29 +2,46 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <std_msgs/Empty.h>
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg)
-{
+ros::Publisher image_freq;
+cv::Mat image, filtered, edges;
+std_msgs::Empty empty_msg;
+
+std::vector<cv::Vec2f> lines;
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg){
   try
   {
-    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-    cv::waitKey(1);
+    image =  cv_bridge::toCvShare(msg, "bgr8")->image;
+
+    cv::blur(image, filtered, cv::Size(3,3));
+    cv::Canny(filtered, edges, 100, 200);
+    cv::HoughLines(edges, lines, 1, CV_PI/180.0, 100);
+
+    image_freq.publish( empty_msg );
+
+//    cv::imshow("view", edges);
+//    cv::waitKey(1);
   }
   catch (cv_bridge::Exception& e)
   {
     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
   }
+
+  ROS_INFO_STREAM("Hough Transform detected " << lines.size());
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "image_listener");
   ros::NodeHandle nh;
+
+  image_freq = nh.advertise<std_msgs::Empty>("image_freq", 10);
   cv::namedWindow("view");
-  cv::startWindowThread();
   image_transport::ImageTransport it(nh);
-  image_transport::Subscriber sub = it.subscribe("camera0/image_raw", 1, imageCallback);
+  image_transport::Subscriber sub = it.subscribe("raspicam_node/image", 1, imageCallback);
   ros::spin();
-  cv::destroyWindow("view");
+  cv::destroyAllWindows();
   return 0;
 }
